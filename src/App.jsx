ff1beda5bc8075c8101028b9715b27e1041208b8
Badr <devbadoory@gmail.com>
2024-10-25 11:58:24 +0300

    add home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Box, Flex } from "@chakra-ui/react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useParams,
+  useLocation,
+} from "react-router-dom";
+import { Box, Flex, Heading } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import Dashboard from "./pages/Dashboard";
 import Tasks from "./pages/Tasks";
 import Projects from "./pages/Projects";
 import TaskManager from "./components/TaskManager/TaskManager";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -15,6 +22,7 @@ function App() {
         <Sidebar />
         <Box flex="1" padding="2rem">
           <Routes>
+            <Route path="/" element={<Home />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/tasks" element={<Tasks />} />
             <Route path="/tasksManager" element={<TaskManager />} />
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import { SimpleGrid, Box, Heading } from "@chakra-ui/react";
+import React from "react";
+import dummyTasks from "../users/dummtTasks";
+
+const Home = () => {
+  return <Box></Box>;
+};
+
+export default Home;
