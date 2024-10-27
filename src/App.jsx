3da56edae99d4411b69c6689b60107f84870e4be
Badr <devbadoory@gmail.com>
2024-10-27 16:15:33 +0300

    Change the layout colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,26 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useParams,
-  useLocation,
-} from "react-router-dom";
-import { Box, Flex, Heading } from "@chakra-ui/react";
-import Navbar from "./components/Navbar";
+import { Box, Flex } from "@chakra-ui/react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
-import Dashboard from "./pages/Dashboard";
-import Tasks from "./pages/Tasks";
-import Projects from "./pages/Projects";
 import TaskManager from "./components/TaskManager/TaskManager";
+import Dashboard from "./pages/Dashboard";
+import EventCalendar from "./pages/EventCalendar";
 import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Tasks from "./pages/Tasks";
 
 function App() {
   return (
     <Router>
-      <Navbar />
       <Flex>
         <Sidebar />
-        <Box flex="1" padding="2rem">
+        <Box flex="1" padding="2rem" bg="white">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/tasks" element={<Tasks />} />
             <Route path="/tasksManager" element={<TaskManager />} />
             <Route path="/projects" element={<Projects />} />
+            <Route path="/calendar" element={<EventCalendar />} />
           </Routes>
         </Box>
       </Flex>
