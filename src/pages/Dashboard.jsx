fbd4651771884979b64a23960c5acc89e3225e02
Badr <devbadoory@gmail.com>
2024-10-29 23:31:54 +0300

    Add daily tasks and tasks card to dashboard page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,6 +2,8 @@ import { Heading, Box, SimpleGrid, GridItem } from "@chakra-ui/react";
 import OverviewCard from "../components/OverviewCard";
 import dummyTasks from "../users/dummyTasks";
 import dummyProjects from "../users/dummyProjects";
+import SmallCalendar from "../components/SmallCalendar";
+import TasksCard from "../components/TasksCard";
 
 const totalTasks = dummyTasks.length;
 const tasksInProgress = dummyTasks.filter(
@@ -16,6 +18,15 @@ const projectsInProgress = dummyProjects.filter(
 const projectsCompleted = dummyProjects.filter(
   (t) => t.status == "Completed"
 ).length;
+
+const today = new Date().toISOString().split("T")[0];
+const dailyTasks = [];
+dummyTasks.forEach((t) => t.start == today && dailyTasks.push(t));
+
+const completedTasks = [];
+dummyTasks.forEach(
+  (t) => t.status == "COMPLETED" && completedTasks.push(t.end)
+);
 
 const Dashboard = () => {
   return (
@@ -37,6 +48,27 @@ const Dashboard = () => {
             inProgress={projectsInProgress}
             title="Projects"
           />
+        </GridItem>
+        <GridItem boxShadow="md" padding={5} borderRadius={7}>
+          <Heading fontSize="2xl" mb={7}>
+            Daily Tasks
+          </Heading>
+          {dailyTasks.map((daily) => (
+            <TasksCard
+              task={daily.task}
+              project={daily.project}
+              priority={daily.priority}
+              status={daily.status}
+              id={daily.id}
+              key={daily.id}
+            />
+          ))}
+        </GridItem>
+        <GridItem boxShadow="md" padding={5} borderRadius={7}>
+          <Heading fontSize="2xl" mb={7}>
+            Calendar
+          </Heading>
+          <SmallCalendar completedTasks={completedTasks} />
         </GridItem>
       </SimpleGrid>
     </Box>
