14061a97faeb3c884b99a9108c91e6854afeb045
Badr <devbadoory@gmail.com>
2024-10-29 23:22:45 +0300

    Add projects overview card

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
-import { Heading, Box, SimpleGrid } from "@chakra-ui/react";
+import { Heading, Box, SimpleGrid, GridItem } from "@chakra-ui/react";
 import OverviewCard from "../components/OverviewCard";
 import dummyTasks from "../users/dummyTasks";
+import dummyProjects from "../users/dummyProjects";
 
 const totalTasks = dummyTasks.length;
 const tasksInProgress = dummyTasks.filter(
@@ -8,17 +9,35 @@ const tasksInProgress = dummyTasks.filter(
 ).length;
 const tasksCompleted = dummyTasks.filter((t) => t.status == "COMPLETED").length;
 
+const totalProjects = dummyProjects.length;
+const projectsInProgress = dummyProjects.filter(
+  (t) => t.status == "In Progress"
+).length;
+const projectsCompleted = dummyProjects.filter(
+  (t) => t.status == "Completed"
+).length;
+
 const Dashboard = () => {
   return (
     <Box>
       <Heading mb={7}>Dashboard</Heading>
       <SimpleGrid columns={{ base: 1, lg: 2 }} spacing={5}>
-        <OverviewCard
-          total={totalTasks}
-          completed={tasksCompleted}
-          inProgress={tasksInProgress}
-          title="Tasks"
-        />
+        <GridItem>
+          <OverviewCard
+            total={totalTasks}
+            completed={tasksCompleted}
+            inProgress={tasksInProgress}
+            title="Tasks"
+          />
+        </GridItem>
+        <GridItem>
+          <OverviewCard
+            total={totalProjects}
+            completed={projectsCompleted}
+            inProgress={projectsInProgress}
+            title="Projects"
+          />
+        </GridItem>
       </SimpleGrid>
     </Box>
   );
