c02c009eac93577e2b586a3a9d508566d4ae18d3
Badr <devbadoory@gmail.com>
2024-10-29 23:15:52 +0300

    Add overview tasks card to dashboard page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,25 @@
-import { Heading, Box } from "@chakra-ui/react";
+import { Heading, Box, SimpleGrid } from "@chakra-ui/react";
+import OverviewCard from "../components/OverviewCard";
+import dummyTasks from "../users/dummyTasks";
+
+const totalTasks = dummyTasks.length;
+const tasksInProgress = dummyTasks.filter(
+  (t) => t.status == "In Progress"
+).length;
+const tasksCompleted = dummyTasks.filter((t) => t.status == "COMPLETED").length;
 
 const Dashboard = () => {
   return (
     <Box>
-      <Heading>Dashboard</Heading>
-      {/* Dashboard content goes here */}
+      <Heading mb={7}>Dashboard</Heading>
+      <SimpleGrid columns={{ base: 1, lg: 2 }} spacing={5}>
+        <OverviewCard
+          total={totalTasks}
+          completed={tasksCompleted}
+          inProgress={tasksInProgress}
+          title="Tasks"
+        />
+      </SimpleGrid>
     </Box>
   );
 };
